9be17d0ca70399877eb93a0f29c4991a06a32185
secretaria72 <ganghasbulla02@gmail.com>
2025-07-13 05:23:33 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-
 // Configuration pour modules activables (signalement, feedback, etc)
 const config = {
   enableReport: false,
   enableFeedback: false,
+  avis_button: true // ✅ Affiche le bouton 💬 si true, sinon il disparaît
 };
