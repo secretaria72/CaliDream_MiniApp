9472ddb16d64befd75f997fef1f82466e8efb6a8
secretaria72 <ganghasbulla02@gmail.com>
2025-07-13 04:31:55 +0200

    Update stock.js

--- config/stock.js
+++ config/stock.js
@@ -12,7 +12,7 @@ fetch('../meta.json')
         stockDiv.textContent = '🟢 En stock';
         stockDiv.classList.add(true);
       } else {
-        stockDiv.textContent = '🔴 Reviens bientôt';
+        stockDiv.textContent = '🔴 Revient bientôt';
         stockDiv.classList.add(false);
       }
     }
