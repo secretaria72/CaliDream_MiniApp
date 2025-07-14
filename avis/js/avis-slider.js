d616c1faa3ccbcd0b5ffd300c5125101dd39c25c
secretaria72 <ganghasbulla02@gmail.com>
2025-07-15 00:33:52 +0200

    Update avis-slider.js

--- avis/js/avis-slider.js
+++ avis/js/avis-slider.js
@@ -1,26 +1,16 @@
-const images = [];
-const folder = 'img/';
+const images = ['img/1.jpg','img/2.jpg','img/3.jpg'];
 let idx = 0;
-
-// Liste dynamique des fichiers (manuellement si pas possible)
-images.push('1.jpg', '2.jpg', '3.jpg');
 
 const slider = document.getElementById('sliderImage');
 const prev = document.getElementById('prevBtn');
 const next = document.getElementById('nextBtn');
 
-function show(i){
-  idx = (i + images.length) % images.length;
-  slider.src = folder + images[idx];
+function update() {
+  slider.src = images[idx];
 }
-
-prev.addEventListener('click', ()=> show(idx - 1));
-next.addEventListener('click', ()=> show(idx + 1));
-
-// tactile pour mobile
-slider.parentElement.addEventListener('click', (e)=> {
-  const w = slider.parentElement.clientWidth;
-  show(e.offsetX < w/2 ? idx - 1 : idx + 1);
+prev.addEventListener('click', ()=> {
+  idx = (idx - 1 + images.length) % images.length; update();
 });
-
-show(0);
+next.addEventListener('click', ()=> {
+  idx = (idx + 1) % images.length; update();
+});
