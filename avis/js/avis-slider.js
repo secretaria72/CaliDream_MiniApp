62201e416460b68f2419f8bc5091711f541e14a7
secretaria72 <ganghasbulla02@gmail.com>
2025-07-14 21:59:57 +0200

    Update avis-slider.js

--- avis/js/avis-slider.js
+++ avis/js/avis-slider.js
@@ -1,31 +1,13 @@
-const imageElement = document.getElementById("slider-img");
-let currentIndex = 0;
-let images = [];
+const images = [
+  "img/avis1.jpg",
+  "img/avis2.jpg",
+  "img/avis3.jpg"
+];
 
-// Charger toutes les images depuis avis/img/
-for (let i = 1; i <= 20; i++) {
-  const img = new Image();
-  img.src = img/avis${i}.jpg;
-  img.onload = () => {
-    images.push(img.src);
-    if (images.length === 1) {
-      imageElement.src = img.src;
-    }
-  };
-}
+let index = 0;
+const imgElement = document.getElementById("photoAvis");
 
-// Navigation
-function showImage(index) {
-  if (images.length > 0) {
-    currentIndex = (index + images.length) % images.length;
-    imageElement.src = images[currentIndex];
-  }
-}
-
-function nextImage() {
-  showImage(currentIndex + 1);
-}
-
-function prevImage() {
-  showImage(currentIndex - 1);
+function changerImage(direction) {
+  index = (index + direction + images.length) % images.length;
+  imgElement.src = images[index];
 }
