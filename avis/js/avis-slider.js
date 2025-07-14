fd42984018706ff41de09db4d11a9a0f75fc688c
secretaria72 <ganghasbulla02@gmail.com>
2025-07-15 00:52:03 +0200

    Update avis-slider.js

--- avis/js/avis-slider.js
+++ avis/js/avis-slider.js
@@ -1,4 +1,4 @@
-const images = ['img/1.jpg','img/2.jpg','img/3.jpg'];
+const images = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg'];
 let idx = 0;
 
 const slider = document.getElementById('sliderImage');
@@ -8,9 +8,11 @@ const next = document.getElementById('nextBtn');
 function update() {
   slider.src = images[idx];
 }
-prev.addEventListener('click', ()=> {
-  idx = (idx - 1 + images.length) % images.length; update();
+prev.addEventListener('click', () => {
+  idx = (idx - 1 + images.length) % images.length;
+  update();
 });
-next.addEventListener('click', ()=> {
-  idx = (idx + 1) % images.length; update();
+next.addEventListener('click', () => {
+  idx = (idx + 1) % images.length;
+  update();
 });
