eddcf34fe7ec0e19d4b99f3e44458487d143b104
secretaria72 <ganghasbulla02@gmail.com>
2025-07-15 21:13:36 +0200

    Update stock.js

--- config/stock.js
+++ config/stock.js
@@ -1,19 +1,1 @@
-fetch('../meta.json')
-  .then(res => res.json())
-  .then(data => {
-    const path = window.location.pathname;
-    const file = path.substring(path.lastIndexOf('/') + 1);
 
-    const product = data.products.find(p => p.filename === file);
-    const stockDiv = document.getElementById('stock-status');
-
-    if (product && stockDiv) {
-      if (product.in_stock) {
-        stockDiv.textContent = '🟢 En stock';
-        stockDiv.classList.add(true);
-      } else {
-        stockDiv.textContent = '🔴 Bientôt';
-        stockDiv.classList.add(false);
-      }
-    }
-  });
